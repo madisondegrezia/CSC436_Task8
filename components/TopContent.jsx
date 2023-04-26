5f8fdbae528130d583278a7df598754efe54114e
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-04-26 11:22:58 -0400

    implementation of next/image

--- components/TopContent.jsx
+++ components/TopContent.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { getCurrentUser } from "../utils/data";
 
 const { name, avatar } = getCurrentUser();
@@ -6,12 +7,12 @@ const TopContent = () => {
   return (
     <div className="flex flex-col items-center mt-6">
       {avatar && (
-        <img
+        <Image
           className="rounded-full mt-10"
           src={avatar}
           alt={name}
-          height="140"
-          width="140"
+          height="145"
+          width="145"
         />
       )}
       {name && <h1 className="h1">{name}</h1>}
